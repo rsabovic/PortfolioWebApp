6353ec564b6d1ec7d27c0073ee05f649e92d0c6d
rsabovic <rejhansabovic@gmail.com>
2026-06-09 01:02:09 -0400

    Changed a few things to work with swag/domain lauch

--- src/components/Directory.tsx
+++ src/components/Directory.tsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 function Directory() {
   return (
     <aside className="directory">
-      <NavLink to="/logIn"><i className="fa-solid fa-arrow-right-to-bracket"></i> Log In </NavLink>
+      <a href="https://authentik.sabovic.net"><i className="fa-solid fa-arrow-right-to-bracket"></i> Log In! </a>
       <NavLink to="/"><i className="fa-solid fa-house-chimney-window"></i> Home!</NavLink>
       <NavLink to="/experience"><i className="fa-regular fa-clipboard"></i> Experience!</NavLink>
       <NavLink to="/education"><i className="fa-solid fa-graduation-cap"></i> Education!</NavLink>
