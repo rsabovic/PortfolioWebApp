cabf4df854e4433159eab73dfb6ed746858bff9f
rsabovic <rejhansabovic@gmail.com>
2026-08-17 19:39:14 -0400

    PP-0011; Reformated CSS and Directory to be mobile friendly

--- src/components/Directory.tsx
+++ src/components/Directory.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -6,6 +7,8 @@ import {
   faGraduationCap,
   faCode,
   faPalette,
+  faBars,
+  faXmark,
 } from '@fortawesome/free-solid-svg-icons'
 import {
   faClipboard,
@@ -13,29 +16,43 @@ import {
 } from '@fortawesome/free-regular-svg-icons'
 
 function Directory() {
+  const [isOpen, setIsOpen] = useState(false)
+  const closeMenu = () => setIsOpen(false)
+
   return (
     <aside className="directory">
-      <a href="https://authentik.sabovic.net">
-        <FontAwesomeIcon icon={faArrowRightToBracket} /> Log In!
-      </a>
-      <NavLink to="/">
-        <FontAwesomeIcon icon={faHouseChimneyWindow} /> Home!
-      </NavLink>
-      <NavLink to="/experience">
-        <FontAwesomeIcon icon={faClipboard} /> Experience!
-      </NavLink>
-      <NavLink to="/education">
-        <FontAwesomeIcon icon={faGraduationCap} /> Education!
-      </NavLink>
-      <NavLink to="/projects">
-        <FontAwesomeIcon icon={faCode} /> Projects!
-      </NavLink>
-      <NavLink to="/contactMe">
-        <FontAwesomeIcon icon={faAddressCard} /> Contact Me!
-      </NavLink>
-      <NavLink to="/themes">
-        <FontAwesomeIcon icon={faPalette} /> Themes!
-      </NavLink>
+      <button
+        className="directoryToggle"
+        onClick={() => setIsOpen(!isOpen)}
+        aria-label="Toggle navigation menu"
+        aria-expanded={isOpen}
+      >
+        <FontAwesomeIcon icon={isOpen ? faXmark : faBars} /> Menu
+      </button>
+
+      <nav className={`directoryLinks ${isOpen ? 'open' : ''}`}>
+        <a href="https://authentik.sabovic.net" onClick={closeMenu}>
+          <FontAwesomeIcon icon={faArrowRightToBracket} /> Log In!
+        </a>
+        <NavLink to="/" onClick={closeMenu}>
+          <FontAwesomeIcon icon={faHouseChimneyWindow} /> Home!
+        </NavLink>
+        <NavLink to="/experience" onClick={closeMenu}>
+          <FontAwesomeIcon icon={faClipboard} /> Experience!
+        </NavLink>
+        <NavLink to="/education" onClick={closeMenu}>
+          <FontAwesomeIcon icon={faGraduationCap} /> Education!
+        </NavLink>
+        <NavLink to="/projects" onClick={closeMenu}>
+          <FontAwesomeIcon icon={faCode} /> Projects!
+        </NavLink>
+        <NavLink to="/contactMe" onClick={closeMenu}>
+          <FontAwesomeIcon icon={faAddressCard} /> Contact Me!
+        </NavLink>
+        <NavLink to="/themes" onClick={closeMenu}>
+          <FontAwesomeIcon icon={faPalette} /> Themes!
+        </NavLink>
+      </nav>
     </aside>
   )
 }
